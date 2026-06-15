fda6982b3e01246ff972e28778757257b2eff8ea
unknown <agnasoftone@gmail.com>
2026-06-15 18:18:30 +0530

    updated project files

--- src/dbcon.php
+++ src/dbcon.php
@@ -7,7 +7,7 @@ if(session_status() == PHP_SESSION_NONE){
 $host=localhost";
 $user="tiadmin";
 $password="tigazh@1234*";
-$database="triqulk6_tiecom1";
+$database="triqulk6_tiecom";
 
 $conn = mysqli_connect($host,$user,$password,$database);
 
